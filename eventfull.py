a56ec57c37e283a2d01d764b3168282b23049a48
juan jose <jjlr1994@gmail.com>
2017-04-17 23:11:57 +0200

    modificacion del .py

--- eventfull.py
+++ eventfull.py
@@ -1,7 +1,6 @@
 import bottle
 from bottle import Bottle,route,run,request,template,static_file
 import json
-import os
 from sys import argv
 import requests
 @route('/',method="get")
@@ -30,5 +29,5 @@ def eventfull():
 def server_static(filepath):
 	return static_file(filepath, root='static')
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
 	run(host='0.0.0.0',port=argv[1])
